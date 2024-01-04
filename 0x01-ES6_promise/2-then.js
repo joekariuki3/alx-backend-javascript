2dec4c584e4bc8cf6fbe2eddbae11b8ddd3576e2
joekariuki3 <joekariuki3@gmail.com>
2024-01-04 13:39:20 +0300

    Catch me if you can! correction

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,6 @@
 export default function handleResponseFromAPI(promise) {
-  return new Promise((resolver, reject) => {
-    if (promise) {
-      resolver({ status: 200, body: 'success' });
-      console.log('Got a response from the API');
-    } else {
-      reject(new Error());
-    }
-  });
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
 }
