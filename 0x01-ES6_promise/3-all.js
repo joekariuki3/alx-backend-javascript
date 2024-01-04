3f36113430d690bb24236b33aac6aa65db26484d
joekariuki3 <joekariuki3@gmail.com>
2024-01-04 13:26:43 +0300

    Handle multiple successful promises correction

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,7 +2,7 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   // Use Promise.all to collectively resolve all promises
-  Promise.all([uploadPhoto(), createUser()])
+  return Promise.all([uploadPhoto(), createUser()])
     .then(([photo, user]) => {
       // Log body (firstName and lastName) to the console
       console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
