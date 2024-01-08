a94b3227af8247964496596bfa8890910a83aaa8
joekariuki3 <joekariuki3@gmail.com>
2024-01-08 21:31:34 +0300

    clean set correction 2

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,9 @@
 export default function cleanSet(set, startString) {
-  if (startString) {
-    const filteredValues = Array.from(set)
-      .filter((value) => value.startsWith(startString))
-      .map((value) => value.substring(startString.length));
-    return filteredValues.join('-');
+  if (!set || !startString) {
+    return '';
   }
+  const filteredValues = Array.from(set)
+    .filter((value) => value.startsWith(startString))
+    .map((value) => value.substring(startString.length));
+  return filteredValues.join('-');
 }
