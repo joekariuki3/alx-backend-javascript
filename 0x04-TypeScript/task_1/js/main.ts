7b30f0d903c96b5f423d8ebef76d5c3973723acd
joekariuki3 <joekariuki3@gmail.com>
2024-01-12 16:26:02 +0300

    Writing a class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,3 +1,4 @@
+// object interface
 interface Teacher{
 	readonly firstName: string;
 	readonly lastName: string;
@@ -6,3 +7,45 @@ interface Teacher{
 	location: string;
 	[propName: string]: any;
 }
+
+// object interface that extends another one
+interface Directors extends Teacher{
+	numberOfReports: number;
+}
+
+// function interface
+interface printTeacherFunction{
+	(fName: string, lName:string): string;
+}
+
+
+const printTeacher: printTeacherFunction = (firstName, lastName) {
+	return (`${firstName[0]}. ${lastName}`)
+}
+
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+const StudentClass: StudentConstructor = class StudentClass implements StudentClassInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+  workOnHomework(): string {
+    return "Currently working";
+  }
+  displayName(): string {
+    return this.firstName;
+  }
+};
+
+export { printTeacher, StudentClass };
