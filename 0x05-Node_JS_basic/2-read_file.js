0d9cde17d31854e88132c7287e99700366ac0312
joekariuki3 <joekariuki3@gmail.com>
2024-02-27 15:29:25 +0300

    Reading a file synchronously with Node JS correction 3

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,52 +1,41 @@
-// read a database file and count number of students
-
 const fs = require('fs');
 
-function countStudents(path) {
-  fs.readFile(path, 'utf8', (error, data) => {
-    // file not present
-    if (error) {
-      throw new Error('Cannot load the database');
+const countStudents = (dataPath) => {
+  if (!fs.existsSync(dataPath)) {
+    throw new Error('Cannot load the database');
+  }
+  if (!fs.statSync(dataPath).isFile()) {
+    throw new Error('Cannot load the database');
+  }
+  const fileLines = fs
+    .readFileSync(dataPath, 'utf-8')
+    .toString('utf-8')
+    .trim()
+    .split('\n');
+  const studentGroups = {};
+  const dbFieldNames = fileLines[0].split(',');
+  const studentPropNames = dbFieldNames.slice(0, dbFieldNames.length - 1);
+
+  for (const line of fileLines.slice(1)) {
+    const studentRecord = line.split(',');
+    const studentPropValues = studentRecord.slice(0, studentRecord.length - 1);
+    const field = studentRecord[studentRecord.length - 1];
+    if (!Object.keys(studentGroups).includes(field)) {
+      studentGroups[field] = [];
     }
-    const studentObjList = [];
-    let studentList = [];
-    let keys = [];
-    // file found fix the data line by line
-    studentList = data.split('\n');
-    // make student object
-    keys = studentList[0].split(',');
-    for (let i = 1; i < studentList.length - 1; i += 1) {
-      const studentInfo = studentList[i].split(',');
-      const student = {};
-      for (let j = 0; j < keys.length; j += 1) {
-        const key = keys[j];
-        const value = studentInfo[j];
-        student[key] = value;
-      }
-      studentObjList.push(student);
-    }
-    console.log(`Number of students: ${studentObjList.length}`);
-    // sort students per field
-    const uniqFields = {};
-    for (let k = 0; k < studentObjList.length; k += 1) {
-      const key = studentObjList[k].field;
-      const value = studentObjList[k].firstname;
-      if (key in uniqFields) {
-        uniqFields[key].push(value);
-      } else {
-        uniqFields[key] = [];
-        uniqFields[key].push(value);
-      }
-    }
-    // list students in each filed
-    const uniqFieldsKeys = Object.keys(uniqFields);
-    for (let l = 0; l < uniqFieldsKeys.length; l += 1) {
-      console.log(
-        `Number of students in ${uniqFieldsKeys[l]}: ${
-          uniqFields[uniqFieldsKeys[l]].length
-        }. List: ${uniqFields[uniqFieldsKeys[l]]}`,
-      );
-    }
-  });
-}
+    const studentEntries = studentPropNames
+      .map((propName, idx) => [propName, studentPropValues[idx]]);
+    studentGroups[field].push(Object.fromEntries(studentEntries));
+  }
+
+  const totalStudents = Object
+    .values(studentGroups)
+    .reduce((pre, cur) => (pre || []).length + cur.length);
+  console.log(`Number of students: ${totalStudents}`);
+  for (const [field, group] of Object.entries(studentGroups)) {
+    const studentNames = group.map((student) => student.firstname).join(', ');
+    console.log(`Number of students in ${field}: ${group.length}. List: ${studentNames}`);
+  }
+};
+
 module.exports = countStudents;
