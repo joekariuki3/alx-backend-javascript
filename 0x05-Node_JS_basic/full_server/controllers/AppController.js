ad03a0f4bdaa6dbcae1ce54989549e0622ae6b3c
joekariuki3 <joekariuki3@gmail.com>
2024-02-27 20:55:00 +0300

    Organize a complex HTTP server using Express

---
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -0,0 +1,9 @@
+class AppController {
+  static getHomepage(req, res) {
+    res.status(200);
+    res.send('Hello Holberton School!');
+  }
+}
+
+export default AppController;
+module.exports = AppController;
