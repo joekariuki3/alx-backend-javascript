3114ff6b4aaac113d3e98ca4983197c09be195ea
joekariuki3 <joekariuki3@gmail.com>
2024-02-27 16:28:57 +0300

    Create a small HTTP server using Node's HTTP module

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+const port = '1245';
+const hostname = '127.0.0.1';
+const responseText = 'Hello Holberton School!\n';
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Length', responseText.length);
+  res.end(responseText);
+});
+app.listen(port, hostname, () => {
+  console.log(`server running at http://${hostname}:${port}/`);
+});
+module.exports = app;
