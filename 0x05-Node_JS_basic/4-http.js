e94fbca102ceaab84bae9768db5960be570413a6
joekariuki3 <joekariuki3@gmail.com>
2024-02-27 16:31:57 +0300

    Create a small HTTP server using Node's HTTP module correction

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const port = '1245';
 const hostname = '127.0.0.1';
-const responseText = 'Hello Holberton School!\n';
+const responseText = 'Hello Holberton School!';
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
