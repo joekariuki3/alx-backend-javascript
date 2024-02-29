36f694bd3a020b5ab4855dbae0ff54e5a15d23f5
joekariuki3 <joekariuki3@gmail.com>
2024-02-29 17:03:21 +0300

    spies correction

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,14 +1,14 @@
-const Utils = require('./utils');
+const Utils = require("./utils");
 const calculateNumber = Utils.calculateNumber;
-const sendPaymentRequestToApi = require('./3-payment.js');
-const sinon = require('sinon');
+const sendPaymentRequestToApi = require("./3-payment.js");
+const sinon = require("sinon");
 const spy = sinon.spy;
-const expect = require('chai').expect;
-describe('sendPaymentRequestToApi', function () {
-  it('calculateNumber method of Utils has been used', () => {
+const expect = require("chai").expect;
+describe("sendPaymentRequestToApi", function () {
+  it("calculateNumber method of Utils has been used", () => {
     const result = spy(Utils);
     sendPaymentRequestToApi(100, 20);
-    expect(result.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
+    expect(result.calculateNumber.calledWith("SUM", 100, 20)).to.be.true;
     expect(result.calculateNumber.callCount).to.be.equal(1);
     result.calculateNumber.restore();
   });
--- 0x06-unittests_in_js/utils.js
+++ 0x06-unittests_in_js/utils.js
@@ -1,17 +1,17 @@
-const Utils =  {
+const Utils = {
   calculateNumber(type, a, b) {
-    if (type === 'SUM') {
+    if (type === "SUM") {
       return Math.round(a) + Math.round(b);
-	}
-    if (type === 'SUBTRACT') {
+    }
+    if (type === "SUBTRACT") {
       return Math.round(a) - Math.round(b);
     }
-    if (type === 'DIVIDE') {
-	  if (Math.round(b) === 0){
-        return 'ERROR'
-	  }
+    if (type === "DIVIDE") {
+      if (Math.round(b) === 0) {
+        return "ERROR";
+      }
       return Math.round(a) / Math.round(b);
-	}
-  }
-}
+    }
+  },
+};
 module.exports = Utils;
