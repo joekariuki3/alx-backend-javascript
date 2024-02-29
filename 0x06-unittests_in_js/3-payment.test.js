039dce223b34b5c2cb507e247d5d964bc2b7a66e
joekariuki3 <joekariuki3@gmail.com>
2024-02-29 17:30:53 +0300

    Spies correction 6

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -3,10 +3,12 @@ const sendPaymentRequestToApi = require("./3-payment.js");
 const sinon = require("sinon");
 const spy = sinon.spy;
 const expect = require("chai").expect;
+
 describe("sendPaymentRequestToApi", function () {
   it("calculateNumber method of Utils has been used", () => {
     const result = spy(Utils);
-    sendPaymentRequestToApi(100, 20);
+    
+	sendPaymentRequestToApi(100, 20);
     expect(result.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
     expect(result.calculateNumber.callCount).to.be.equal(1);
     result.calculateNumber.restore();
--- 0x06-unittests_in_js/utils.js
+++ 0x06-unittests_in_js/utils.js
@@ -1,5 +1,5 @@
 const Utils = {
-	calculateNumber: function (type, a, b) {
+  calculateNumber (type, a, b) {
     if (type === "SUM") {
       return Math.round(a) + Math.round(b);
     }
@@ -12,6 +12,7 @@ const Utils = {
       }
       return Math.round(a) / Math.round(b);
     }
+    return 0;
   },
 };
 module.exports = Utils;
