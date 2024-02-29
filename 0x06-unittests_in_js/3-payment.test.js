7692944060d543b67f6185f604c0839b96388386
joekariuki3 <joekariuki3@gmail.com>
2024-02-29 17:18:47 +0300

    spies correction 4

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -7,7 +7,7 @@ describe("sendPaymentRequestToApi", function () {
   it("calculateNumber method of Utils has been used", () => {
     const result = spy(Utils);
     sendPaymentRequestToApi(100, 20);
-    expect(result.calculateNumber.calledWith("SUM", 100, 20)).to.be.true;
+    expect(result.Utils.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
     expect(result.calculateNumber.callCount).to.be.equal(1);
     result.calculateNumber.restore();
   });
